7e570aacc4ac2caef932b476221fc27c10a6f56c
Syed Iqram <syediqram82@gmail.com>
2024-04-19 18:10:14 +0530

    Users and Products working

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CenterBox, Box, Row, Column, Text, HoverBox } from "../styled";
 import { SideBarSection } from "./components";
