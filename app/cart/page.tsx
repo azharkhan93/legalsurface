bb0ef542417439771a4e09aa5e3f2a5879bd646e
azharkhan93 <azharkhan.work101@gmail.com>
2024-08-10 01:56:35 +0530

    new changes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,7 +7,9 @@ export default function Page() {
   const { cart, removeFromCart } = useCart();
 
   const calculateGrandTotal = () => {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+    return cart
+      .reduce((total, item) => total + item.price * item.quantity, 0)
+      .toFixed(2);
   };
 
   return (
@@ -17,82 +19,77 @@ export default function Page() {
         aboutText={"Cart"}
         whoWeAreText={"View Cart"}
       />
-      
-      <Column  alignItems="center" 
-      gap={"xl"}
-      
-      >
-        <Text variant="heading" textAlign={"start"} mt={"xl"}>Shopping Cart</Text>
+
+      <Column alignItems="center" gap={"xl"}>
+        <Text variant="heading" textAlign={"start"} mt={"xl"}>
+          Shopping Cart
+        </Text>
         <Row
-        width="100%" 
+          width="100%"
           justifyContent={"center"}
           flexWrap={"wrap"}
           gap={"xl"}
-
-          alignItems="flex-start"  
+          alignItems="flex-start"
         >
           {cart.length > 0 ? (
             cart.map((item, index) => (
               <Box
-              width={"350px"}
-              px={"s"}
+                width={"350px"}
+                px={"s"}
                 key={index}
                 border="1px solid gray"
-                
-               flexDirection="column"
+                flexDirection="column"
                 gap="l"
                 py={"m"}
               >
-                <Row
-                alignItems={"center"}
-                justifyContent={"space-between"}
-                >
-                <Text variant="subHeading" fontWeight={"regular"}>{item.productName}</Text>
-                <Text variant="body">Price: {item.price} /Rs</Text>
+                <Row alignItems={"center"} justifyContent={"space-between"}>
+                  <Text variant="subHeading" fontWeight={"regular"}>
+                    {item.productName}
+                  </Text>
+                  <Text variant="body">Price: {item.price} /Rs</Text>
                 </Row>
-                <Row
-                alignItems={"center"}
-                justifyContent={"space-between"}
-                >
-                <Text variant="body">Quantity: {item.quantity}</Text>
-                <Button
-                  width="140px"
-                  variant="outline"
-
-                  onClick={() => removeFromCart(item.productName)}
-                >
-                  Remove from Cart
-                </Button>
+                <Row alignItems={"center"} justifyContent={"space-between"}>
+                  <Text variant="body">Quantity: {item.quantity}</Text>
+                  <Button
+                    width="140px"
+                    variant="outline"
+                    onClick={() => removeFromCart(item.productName)}
+                  >
+                    Remove from Cart
+                  </Button>
                 </Row>
               </Box>
             ))
           ) : (
-            <Text variant="subHeading" py={"l"}>Your cart is empty</Text>
+            <Text variant="subHeading" py={"l"}>
+              Your cart is empty
+            </Text>
           )}
         </Row>
         {cart.length > 0 ? (
           <>
-            <Box width="90%"  textAlign="center"
-            py={"xl"}
-            alignItems={"flex-end"}
-            gap={"xl"}
+            <Box
+              width="90%"
+              textAlign="center"
+              py={"xl"}
+              alignItems={"flex-end"}
+              gap={"xl"}
             >
               <Text variant="subHeading">
                 Grand Total: {calculateGrandTotal()} /Rs
               </Text>
-              <Button variant="outline" width={"24%"} 
-              height={"35px"} onClick={() => alert("Proceed to Checkout")}
+              <Button
+                variant="outline"
+                width={"24%"}
+                height={"35px"}
+                onClick={() => alert("Proceed to Checkout")}
               >
-              Checkout
-            </Button>
+                Checkout
+              </Button>
             </Box>
-            
           </>
         ) : null}
       </Column>
     </>
   );
 }
-
-
-
